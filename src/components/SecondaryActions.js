e676388c6668fb8efe7aec8a9f73a7b6ef4c0a86
Lucas Munoz <lucas.munoz@globallogic.com>
2021-10-20 11:39:09 -0300

    btn clear board

--- src/components/SecondaryActions.js
+++ src/components/SecondaryActions.js
@@ -1,8 +1,31 @@
-const SecondaryActions = () => (
-    <div className="secondary-actions"> 
-        <button className="btn-solve-selected">Solve Selected</button>
-        <button className="btn-clear">Clear Board</button>
-    </div>
-)
+import { useDispatch } from 'react-redux';
+import { setBoard } from '../redux/actions/boardActions';
+
+const SecondaryActions = () => {
+
+    const dispatch = useDispatch();
+
+    const handleClearBoard = () => {
+        const initialBoard = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ];
+        dispatch(setBoard(initialBoard));
+    }
+
+    return (
+        <div className="secondary-actions">
+            <button className="btn-solve-selected">Solve Selected</button>
+            <button className="btn-clear" onClick={handleClearBoard}>Clear Board</button>
+        </div>
+    )
+}
 
 export default SecondaryActions;
